a7cf881da9dc7e93d1eeded399dc444b2f3de138
Aimurzin Ainur <aliveairush@gmail.com>
2022-01-10 14:01:35 +0300

    Module 1. Task 1) Hello world, alert(), prompt();

---
+++ module1/task_01_hello_world/first_project/js-info.js
@@ -0,0 +1,5 @@
+alert("JavaScript разработал Brendan Eich");
+alert("JavaScript разработан за 10 дней");
+alert("JavaScript появился в 1995 году");
+alert("JavaScript это не Java");
+alert("Как называется спецификация JavaScript? ECMAScript");
