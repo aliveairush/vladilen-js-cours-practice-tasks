852775e28ffcc92b8302981f09d036303c53faa1
Aimurzin Ainur <aliveairush@gmail.com>
2022-01-11 18:53:39 +0300

    Module 1. Task 3)  Data types

---
+++ module1/block_03_data_types/data-types.js
@@ -0,0 +1,18 @@
+const name = 'Dipper',
+  iq = 5,
+  isDipperFictional = true,
+  car = {name: "Porsche"},
+  myFuture = undefined,
+  myMoney = null,
+  bigNumber = 100n,
+  symbol = Symbol('(C)');
+
+
+console.log("Name", name, typeof name);
+console.log("IQ", iq, typeof iq);
+console.log("Is dipper fictional", isDipperFictional, typeof isDipperFictional);
+console.log("Car", car, typeof car);
+console.log("My future", myFuture, typeof myFuture);
+console.log("My money", myMoney, typeof myMoney);
+console.log("Big number", bigNumber, typeof bigNumber);
+console.log("Symbol", symbol, typeof symbol);
---
+++ module1/block_03_data_types/index.js
@@ -0,0 +1,9 @@
+let myNumber = 5,
+  myString = 'Special kid',
+  myBoolean = true;
+
+myNumber = 'Need to build a wall';
+myString = 10;
+myBoolean = null;
+
+alert(`Number: ${myNumber}\nString: ${myString}\nBoolean: ${myBoolean}`);
