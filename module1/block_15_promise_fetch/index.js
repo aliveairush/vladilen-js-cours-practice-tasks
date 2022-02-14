f4db85bf028bcd218eb66d7c0c6818c90f2b63d1
Aimurzin Ainur <aliveairush@gmail.com>
2022-02-14 12:28:03 +0300

    Block 15 tasks. Doing fetch request and appending data to the page.

---
+++ module1/block_15_promise_fetch/index.js
@@ -0,0 +1,86 @@
+const USER_LIST_URL = "https://jsonplaceholder.typicode.com/users";
+const PHOTOS_URL = "https://jsonplaceholder.typicode.com/photos"
+
+const dataContainerElem1 = document.querySelector("#data-container-1");
+const dataContainerElem2 = document.querySelector("#data-container-2");
+const dataContainerElem3 = document.querySelector("#data-container-3");
+
+// 🚩 TASK 1
+
+const generateLoader = () => {
+  const span = document.createElement('span');
+  span.className = 'loader';
+  span.textContent = 'Загрузка...';
+  return span;
+}
+
+const displayLoader = (container, display = false) => {
+  if (display) {
+    container?.append(generateLoader());
+  }
+  else {
+    const loaderElem = container?.querySelector(".loader");
+    loaderElem?.remove();
+  }
+}
+
+const transformUserListIntoHtml = (userList = []) => userList.reduce((html, user) => html + `<li><a href="#">${user.name}</a></li>`, '');
+
+const loadAllUsers = () => {
+  displayLoader(dataContainerElem1, true);
+
+  fetch(USER_LIST_URL)
+    .finally(() => displayLoader(dataContainerElem1, false))
+    .then((response) => {
+      if (!response.ok) throw new Error("Something went wrong, could not fetch data");
+      return response.json();
+    })
+    .then(userList => dataContainerElem1.insertAdjacentHTML("beforeend", transformUserListIntoHtml(userList)))
+    .catch(error => console.error(error.message));
+}
+
+loadAllUsers();
+
+// TASK 2 🚩
+const getUsersByIds = (ids = []) => {
+  displayLoader(dataContainerElem2, true);
+
+  const requests = ids.map(id => fetch(`${USER_LIST_URL}/${id}`));
+
+  Promise.all(requests)
+    .finally(() => displayLoader(dataContainerElem2, false))
+    .then(responses => Promise.all(responses.map(response => response.json())))
+    .then(responseJsonList => dataContainerElem2.insertAdjacentHTML("beforeend", transformUserListIntoHtml(responseJsonList)))
+    .catch(error => console.error(error.message));
+}
+
+getUsersByIds([5, 6, 2, 1]);
+
+
+// TASK 3 🚩
+
+const transformPhotoIntoHtml = ({url, title} = {}) => {
+  return `<div class="photo-item">
+    <img alt="" class="photo-item__image" src="${url}">
+      <h3 class="photo-item__title">
+        ${title}
+      </h3>
+  </div>`
+}
+
+const getFastestLoadedPhoto = (ids) => {
+  displayLoader(dataContainerElem3, true);
+
+  const requests = ids.map(photoId => fetch(`${PHOTOS_URL}/${photoId}`));
+
+  Promise.race(requests)
+    .finally(() => displayLoader(dataContainerElem3, false))
+    .then(response => response.json())
+    .then(photo => {
+      console.log(photo);
+      dataContainerElem3.insertAdjacentHTML("beforeend", transformPhotoIntoHtml(photo));
+    })
+    .catch(error => console.error(error.message));
+}
+
+getFastestLoadedPhoto([60, 12, 55])
